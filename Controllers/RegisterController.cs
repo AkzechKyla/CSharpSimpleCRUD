3ff2bd839786d3285716c6cb6912332c59de0e15
Kyla <kflmorcillos@gmail.com>
2025-01-25 00:05:39 +0800

    Implement Registration page

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using CSharpSimpleCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpSimpleCRUD.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            ViewBag.SuccessMessage = "Your form has been submitted successfully.";
+            ModelState.Clear();
+            return View();
+        }
+    }
+}
---
+++ Models/RegisterModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSharpSimpleCRUD.Models
+{
+    public class RegisterModel
+    {
+        public int ParticipantId { get; set; }
+
+        [Required(ErrorMessage = "The First Name is required.")]
+        public string FirstName { get; set; } = "";
+
+        [Required(ErrorMessage = "The Last Name is required.")]
+        public string LastName { get; set; } = "";
+
+        [Required(ErrorMessage = "The Address is required.")]
+        public string Address { get; set; } = "";
+
+        [Required(ErrorMessage = "The Phone Number is required.")]
+        public string PhoneNumber { get; set; } = "";
+
+        [Required(ErrorMessage = "The E-mail Address is required.")]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Required(ErrorMessage = "The Birth Date is required.")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "The Gender is required.")]
+        public string Gender { get; set; } = "";
+
+        [Required(ErrorMessage = "The T-shirt Size is required.")]
+        public string TShirtSize { get; set; } = "";
+    }
+}

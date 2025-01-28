fc899e9299f2523ed878909e7282eab127986116
Kyla <kflmorcillos@gmail.com>
2025-01-28 18:49:49 +0800

    Show Participants List

---
+++ Controllers/ParticipantsController.cs
@@ -0,0 +1,15 @@
+﻿using CSharpSimpleCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace CSharpSimpleCRUD.Controllers
+{
+    public class ParticipantsController : Controller
+    {
+        public IActionResult Index(ParticipantModel participantModel)
+        {
+            List<ParticipantModel> participants = participantModel.FetchFromDatabase();
+            return View(participants);
+        }
+    }
+}

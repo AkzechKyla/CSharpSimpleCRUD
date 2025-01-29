1cc97c0ab4d8e3757f463fcba939d244c2a7c069
Kyla <kflmorcillos@gmail.com>
2025-01-29 15:54:22 +0800

    Add DbContexts

---
+++ DbContexts/DataContext.cs
@@ -0,0 +1,14 @@
+﻿global using Microsoft.EntityFrameworkCore;
+using CSharpSimpleCRUD.Entities;
+
+namespace CSharpSimpleCRUD.DbContexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Event> Events => Set<Event>();
+    } 
+}

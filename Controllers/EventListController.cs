25bd4b3e73c01ca99515a62c7f55124d3b1b038d
Kyla <kflmorcillos@gmail.com>
2025-01-29 16:45:28 +0800

    Implement Event list page

---
+++ Controllers/EventListController.cs
@@ -0,0 +1,16 @@
+﻿using CSharpSimpleCRUD.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpSimpleCRUD.Controllers
+{
+    public class EventListController(DataContext context) : Controller
+    {
+        private readonly DataContext _context = context;
+
+        public IActionResult Index()
+        {
+            var events = _context.Events.ToList();
+            return View(events);
+        }
+    }
+}
